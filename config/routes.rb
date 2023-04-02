619d9e706ba11b70f7caa54df4985141904ed091
Ryotaro Inoue <sherlotuq@gmail.com>
2023-04-02 14:40:14 -0400

    refactor:reorganize the structure of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define application routes using the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Define routes for users with only show and create actions
+  # Define routes for User resources
   resources :users, only: %i[show create] do
+    # Define member routes for User resources
     member do
-      get 'followers' # Route for getting followers of a user
-      get 'following' # Route for getting users being followed by a user
+      # Route for getting a list of a user's followers
+      get 'followers'
+
+      # Route for getting a list of users being followed by a user
+      get 'following'
+
+      # Route for getting sleep records of a user's friends (mutual followers)
+      get 'friends_sleep_records'
     end
-    resources :sleep_records, only: %i[index create] # Nested route for sleep records of a user
-    get 'friends_sleep_records', on: :member # Route for getting sleep records of a user's friends
+
+    # Nested routes for SleepRecord resources under User resources
+    resources :sleep_records, only: %i[index create]
   end
 
-  # Define routes for creating and destroying relationships
+  # Define routes for creating and destroying Relationship resources
   resources :relationships, only: %i[create destroy]
 end
