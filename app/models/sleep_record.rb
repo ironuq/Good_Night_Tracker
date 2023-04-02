595e85e295c2877e0d23bb27240b15bf7f5d5dbd
Ryotaro Inoue <sherlotuq@gmail.com>
2023-04-01 22:02:43 -0400

    feat:Implement SleepRecord model

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
+# SleepRecord model represents a sleep record for a user.
+# It stores the start and end times of a sleep session and is associated with a user.
 class SleepRecord < ApplicationRecord
+  # Each sleep record belongs to a user
   belongs_to :user
+
+  # Validate the presence of start_time and end_time attributes
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
