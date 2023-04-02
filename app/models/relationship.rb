63feb2fe257b3ca1e17129371b58cc396cefc3fa
Ryotaro Inoue <sherlotuq@gmail.com>
2023-04-01 22:06:37 -0400

    feat:Implement Relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
+# Relationship model represents the follower-followed relationship between users.
+# It stores the follower and followed user IDs and ensures their presence.
 class Relationship < ApplicationRecord
+  # Each relationship has a follower (User) and a followed (User)
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followed, class_name: 'User'
+
+  # Validate the presence of follower_id and followed_id attributes
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
