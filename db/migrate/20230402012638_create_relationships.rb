94004306a2666bdad8af55f4d366b5400635b9a3
Ryotaro Inoue <sherlotuq@gmail.com>
2023-04-01 22:14:37 -0400

    feat:add index and unique constraint to follower and followed id

--- db/migrate/20230402012638_create_relationships.rb
+++ db/migrate/20230402012638_create_relationships.rb
@@ -1,4 +1,9 @@
-class CreateRelationships < ActiveRecord::Migration[7.0]
+# frozen_string_literal: true
+
+# CreateRelationships migration creates the relationships table in the database.
+# It defines the follower_id, followed_id, and timestamps columns.
+# It also adds indices for follower_id, followed_id, and a unique index for the combination of both.
+class CreateRelationships < ActiveRecord::Migration[6.0]
   def change
     create_table :relationships do |t|
       t.integer :follower_id
@@ -6,5 +11,10 @@ class CreateRelationships < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+    # Add an index to improve query performance for follower_id and followed_id
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    # Add a unique index to ensure a follower can only follow a user once
+    add_index :relationships, %i[follower_id followed_id], unique: true
   end
 end
