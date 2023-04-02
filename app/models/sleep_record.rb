1cf0d62650234f4a112cd7e1a104f0ca716853a0
Ryotaro Inoue <sherlotuq@gmail.com>
2023-04-02 16:43:35 -0400

    fix:changed error message for the validation in SleepReord model

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -16,6 +16,6 @@ class SleepRecord < ApplicationRecord
 
   # Custom validation for checking if start_time is before end_time
   def start_time_must_be_before_end_time
-    errors.add(:start_time, 'must be before end_time') if start_time >= end_time
+    errors.add(:start_time, 'start_time must be before end_time') if start_time >= end_time
   end
 end
